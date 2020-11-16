d410bce4272c0f467be624affd053fdaeedbb6fb
Connor Skennerton <c.skennerton@gmail.com>
2020-11-15 20:29:45 -0800

    add in failing test for when S and P attributes are set

--- parse_test.go
+++ parse_test.go
@@ -99,3 +99,73 @@ func TestParseOneRuleWithAttributeLocalFiles(t *testing.T) {
 		}
 	}
 }
+
+// Test a mkfile with a single rule. The target has a single
+// prerequesite; both are local files. Uses an alternative
+// method to determine if the target is up to date.
+func TestParseOneRuleAltCompareLocalFiles(t *testing.T) {
+	mkfileAsString := "somefile.txt:Pcmp -s: a_prereq.csv\n\techo $target"
+	ruleSet := parse(mkfileAsString, "mkfile", "/mkfile")
+	if len(ruleSet.rules) != 1 {
+		t.Errorf("There should be 1 rule")
+	}
+	rule := ruleSet.rules[0]
+	if len(rule.prereqs) != 1 {
+		t.Errorf("There should be 1 prerequesite")
+	}
+	if rule.prereqs[0] != "a_prereq.csv" {
+		t.Errorf("The prerequesites are not a_prereq.csv")
+	}
+	if rule.command[0] != "cmp" || rule.command[1] != "-s" {
+		t.Error("The rule's comparison command ", rule.command, " is not cmp -s")
+	}
+	ruleAttributesNotSet(t, &rule)
+}
+
+// Test a mkfile with a single rule. The target has a single
+// prerequesite; both are local files. Uses an alternative shell
+// to run the recipie
+func TestParseOneRuleAltShellLocalFiles(t *testing.T) {
+	mkfileAsString := "somefile.txt:Scmp -s: a_prereq.csv\n\techo $target"
+	ruleSet := parse(mkfileAsString, "mkfile", "/mkfile")
+	if len(ruleSet.rules) != 1 {
+		t.Errorf("There should be 1 rule")
+	}
+	rule := ruleSet.rules[0]
+	if len(rule.prereqs) != 1 {
+		t.Errorf("There should be 1 prerequesite")
+	}
+	if rule.prereqs[0] != "a_prereq.csv" {
+		t.Errorf("The prerequesites are not a_prereq.csv")
+	}
+	if rule.shell[0] != "cmp" || rule.shell[1] != "-s" {
+		t.Error("The rule's comparison command ", rule.shell, " is not cmp -s")
+	}
+	ruleAttributesNotSet(t, &rule)
+}
+
+// Test a mkfile with a single rule. The target has a single
+// prerequesite; both are local files. Tries to set both an
+// alternative shell and an alternative comparion - this will
+// fail as S and P attributes are mutually exclusive
+func TestParseOneRuleAltShellAltCmpLocalFiles(t *testing.T) {
+	mkfileAsString := "somefile.txt:Scmp -s Pcmp -s: a_prereq.csv\n\techo $target"
+	ruleSet := parse(mkfileAsString, "mkfile", "/mkfile")
+	if len(ruleSet.rules) != 1 {
+		t.Errorf("There should be 1 rule")
+	}
+	rule := ruleSet.rules[0]
+	if len(rule.prereqs) != 1 {
+		t.Errorf("There should be 1 prerequesite")
+	}
+	if rule.prereqs[0] != "a_prereq.csv" {
+		t.Errorf("The prerequesites are not a_prereq.csv")
+	}
+	if len(rule.shell) != 2 {
+		t.Error("there are extra elements in the shell", rule.shell)
+	}
+	if rule.shell[0] != "cmp" || rule.shell[1] != "-s" {
+		t.Error("The rule's comparison command ", rule.shell, " is not cmp -s")
+	}
+	ruleAttributesNotSet(t, &rule)
+}
